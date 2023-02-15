94e461552807dcb88e389004197835bb00e47004
ThanhKun <nguyendinhthanh23102001@gmail.com>
2023-02-15 16:28:58 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,24 @@
 $(function () {
   AOS.init();
 
-  $(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        (this.scrollY > 20) ? $('.banner').addClass("show-index") : $('.banner').removeClass("show-index")
-        if(this.scrollY > 500){
-            $('.navbar-sticky').addClass("sticky");
-            $('.nav-menu-header').removeClass('logo-dark')
-        }else{
-            $('.navbar-sticky').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      // sticky navbar on scroll script
+      // scroll-up button show/hide script
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
     });
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      // removing smooth scroll on slide-up button click
+      $("html").css("scrollBehavior", "auto");
     });
+  });
 });
-});
-
-
-
-
-
-
 
 $(".banner-image").slick({
   infinite: true,
@@ -40,6 +26,33 @@ $(".banner-image").slick({
   slidesToScroll: 1,
   arrows: true,
   dots: true,
+  speed: 300,
+  infinite: true,
+  autoplaySpeed: 5000,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 1230,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+  ],
 });
 
 $(".multiple-items").slick({
@@ -48,38 +61,64 @@ $(".multiple-items").slick({
   slidesToScroll: 1,
   arrows: true,
   dots: true,
+  speed: 300,
+  infinite: true,
+  autoplaySpeed: 5000,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 1230,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+  ],
+});
+
+$(".text-slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  speed: 300,
+  infinite: true,
+  autoplaySpeed: 5000,
+  autoplay: true,
   responsive: [
     {
       breakpoint: 575,
       settings: {
         arrows: false,
-      } 
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 1230,
+      settings: {
+        arrows: false,
+        dots: false,
+      },
     },
   ],
 });
-
-//   $('.my-slider').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     arrows: true,
-//     dots: true,
-//     speed: 300,
-//     infinite: true,
-//     autoplaySpeed: 5000,
-//     autoplay: true,
-//     responsive: [
-//   {
-//     breakpoint: 991,
-//     settings: {
-//       slidesToShow: 3,
-//     }
-//   },
-//   {
-//     breakpoint: 767,
-//     settings: {
-//       slidesToShow: 3,
-//     }
-//   },
-
-// ]
-//   });
